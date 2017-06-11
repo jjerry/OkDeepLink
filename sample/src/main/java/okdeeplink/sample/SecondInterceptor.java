7d10cdb62739c6123dd65a38489716a2b4489749
hongjun <zhangqijun@51zhangdan.com.cn>
2017-06-12 00:48:10 +0800

    [MDF]modify preview png

--- sample/src/main/java/okdeeplink/sample/SecondInterceptor.java
+++ sample/src/main/java/okdeeplink/sample/SecondInterceptor.java
@@ -5,8 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 
-import java.util.Map;
-
 import okdeeplink.Intercept;
 import okdeeplink.Interceptor;
 import okdeeplink.Request;
@@ -25,23 +23,19 @@ public class SecondInterceptor extends Interceptor {
         Context context = request.getContext();
 
         StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("拦截到你想打开\n");
+        stringBuffer.append("Intercept\n");
         stringBuffer.append("URL: " + request.getUrl() + "\n");
 
-        Map<String, Object> query = request.getQuery();
-        for (String key : query.keySet()) {
-            stringBuffer.append(key + ":" + query.get(key) + "\n");
-        }
         AlertDialog.Builder builder = new AlertDialog.Builder(context,R.style.Theme_AppCompat_Dialog_Alert);
-        builder.setTitle("提醒");
+        builder.setTitle("Notice");
         builder.setMessage(stringBuffer);
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 call.cancel();
             }
         });
-        builder.setPositiveButton("加点盐", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 intent.putExtra("key1", "value3");
